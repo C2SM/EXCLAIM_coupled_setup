3b389b5dbfa26c3214b1d3cf000f313ae0199323
Mikael Stellio <mikael.stellio@c2sm.ethz.ch>
2025-09-29 17:42:15 +0200

    Improved GPU setup

--- run_utils/hybrid_wrapper.sh
+++ run_utils/hybrid_wrapper.sh
@@ -12,19 +12,16 @@
 
 export LOCAL_RANK=$SLURM_LOCALID
 export GLOBAL_RANK=$SLURM_PROCID
-# The first 4 ranks get assigned to GPUs
-# Subsequent ranks get no GPU
-export GPUS=(0 1 2 3 0 1 2 3)
-export NUMA=(0 1 2 3 0 1 2 3)
-export NUMA_NODE=${NUMA[$LOCAL_RANK]}
 
-export CUDA_VISIBLE_DEVICES=${GPUS[$SLURM_LOCALID%8]}
+export NUMA_IDS=(0 1 2 3)
+export NUMA_NODE=${NUMA_IDS[$LOCAL_RANK % 4]}
+
+export CUDA_VISIBLE_DEVICES=$(($LOCAL_RANK % 4))
 
 export MPICH_GPU_SUPPORT_ENABLED=1
+export MPICH_GPU_IPC_ENABLED=0	
 
-#if [[ $LOCAL_RANK -lt 4 ]]; then
-#  export MPICH_GPU_SUPPORT_ENABLED=1
-#fi
+ulimit -s unlimited
 
 if [[ $# -lt 2 ]]; then
     # only ICON binary as argument

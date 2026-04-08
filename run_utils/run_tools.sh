2de58d8567e8044fd79f4450fe44f0e71acda498
Michael Jähn <mjaehn@ethz.ch>
2026-04-08 10:48:20 +0200

    Add export PMI_MMAP_SYNC_WAIT_TIME=300

--- run_utils/run_tools.sh
+++ run_utils/run_tools.sh
@@ -96,6 +96,7 @@ set_environment(){
        export ICON4PY_WAIT_FOR_COMPILATION=1
        export DACE_compiler_cuda_block_size_limit=256
        export PY2FGEN_LOG_LEVEL=WARNING
+       export PMI_MMAP_SYNC_WAIT_TIME=300
        export HWMALLOC_LARGE_LIMIT=$((1 << 26)) # 64 MiB, default 2 MiB
        export HWMALLOC_LARGE_SEGMENT_SIZE=$((1 << 26)) # 64 MiB, default 2 MiB
        export HWMALLOC_NEVER_FREE=1 # This should make sure that even if halos are bigger than the above options, the allocations are still kept around 
